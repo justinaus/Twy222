cbaab39b6be393eb12fc1f0799a39fef8b3b374b
Bonkook Koo <djmimi@nate.com>
2019-01-04 16:23:09 +0900

    Add kakao region api

--- Twy222/Services/KakaoApiService.swift
+++ Twy222/Services/KakaoApiService.swift
@@ -7,3 +7,104 @@
 //
 
 import Foundation
+
+final class KakaoApiService {
+    static let shared = KakaoApiService();
+    
+    public func getGridModelByLonLat( lon: Double, lat: Double, callback:@escaping ( GridModel? ) -> Void ) {
+        let url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=\(lon)&y=\(lat)";
+        
+        guard let urlObjct = URL(string: url) else {
+            callback( nil );
+            return;
+        }
+        
+        var request = URLRequest(url: urlObjct );
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK \(KakaoConfig.APP_KEY)", forHTTPHeaderField: "Authorization");
+        
+        let currentTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            if (error != nil) {
+                print(error!);
+                callback( nil );
+            } else {
+                guard let json = try? JSONSerialization.jsonObject(with: data!, options: []) else {
+                    callback( nil );
+                    return;
+                }
+                
+                guard let jsonStringAny = json as? [String:Any] else {
+                    callback( nil );
+                    return;
+                }
+                
+                guard let gridModel = self.parseJson( json: jsonStringAny ) else {
+                    callback( nil );
+                    return;
+                }
+                
+                callback( gridModel );
+            }
+        }
+        
+        currentTask.resume()
+    }
+    
+    private func parseJson( json: [String:Any] ) -> GridModel? {
+        guard let documents = json[ "documents" ] as? Array< [ String : Any ] > else {
+            return nil;
+        }
+        
+        let len = documents.count;
+        
+        for i in 0..<len {
+            let obj = documents[ i ];
+            
+            guard let region_type = obj[ "region_type" ] as? String else {
+                continue;
+            }
+            
+            // 일단 법정 타입 데이터로.
+            if( region_type == "H" ) {
+                continue;
+            }
+            
+            guard let gridModel = makeGridModel( obj ) else {
+                continue;
+            }
+            
+            return gridModel;
+        }
+        
+        return nil;
+    }
+    
+    private func makeGridModel( _ obj: [ String : Any ] ) -> GridModel? {
+        guard let code = obj[ "code" ] as? String else {
+            return nil;
+        }
+        guard let x = obj[ "x" ] as? Double else {
+            return nil;
+        }
+        guard let y = obj[ "y" ] as? Double else {
+            return nil;
+        }
+        
+        var dongName:String?;
+        
+        if let region_3depth_name = obj[ "region_3depth_name" ] as? String {
+            dongName = region_3depth_name;
+        } else if let address_name = obj[ "address_name" ] as? String {
+            dongName = address_name;
+        }
+        
+        if( dongName == nil ) {
+            return nil;
+        }
+        
+        let gridModel:GridModel = GridModel( id: code, lat: String(y), lon: String(x), dongName: dongName! );
+        
+        return gridModel;
+    }
+    
+}
--- Twy222/ViewController.swift
+++ Twy222/ViewController.swift
@@ -13,10 +13,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager: CLLocationManager = CLLocationManager();
     var currentLocation: CLLocation?;
+    
+    var dateLastCalled:Date?;
+    let LIMIT_INTERVAL_MINUTES_TO_CALL:Int = 5;
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("view did load");
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
@@ -37,16 +40,55 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else {
-//            goJustDefaultTempLocation();
-            
             return;
         }
         
         currentLocation = location;
+//        print("locationManager", location.coordinate.longitude, location.coordinate.latitude );
         
-        print("locationManager", location.coordinate.longitude, location.coordinate.latitude );
+        tryStartToApiCall();
+    }
+    
+    func tryStartToApiCall() {
+        let now: Date = Date();
         
-        //        getGridModelByLonLat( lon: location.coordinate.longitude, lat: location.coordinate.latitude );
+        if( dateLastCalled != nil) {
+            let componenets = Calendar.current.dateComponents([.minute], from: dateLastCalled!, to: now);
+            
+            if( componenets.minute ?? 0 < LIMIT_INTERVAL_MINUTES_TO_CALL ) {
+//                print("기존에 콜 한지 xx분도 안됨, 아무것도 안함")
+                return;
+            }
+        }
+        
+        dateLastCalled = now;
+        
+        getGridModelByLonLat( lon: currentLocation!.coordinate.longitude, lat: currentLocation!.coordinate.latitude );
+    }
+    
+    func getGridModelByLonLat( lon: Double, lat: Double ) {
+        KakaoApiService.shared.getGridModelByLonLat(lon: lon, lat: lat) { ( gridModel: GridModel? ) in
+            if( gridModel == nil ) {
+                return;
+            }
+            
+            print(gridModel!.dongName)
+            
+//            GridManager.shared.addGrid(model: model);
+//
+//            let result = FronteerKr.convertGRID_GPS( toGrid: true, lat_X: lat, lng_Y: lon );
+//            model.setKmaXY(nX: result.x, nY: result.y);
+//
+//            // 기상청 기준 좌표 :  62, 122
+//
+//            let dateNow = Date();
+//
+//            guard let gridModel = GridManager.shared.getCurrentGridModel() else {
+//                return;
+//            }
+//
+//            self.getForecastVeryShortModel(dateNow: dateNow, gridModel: gridModel)
+        }
     }
 }
 
