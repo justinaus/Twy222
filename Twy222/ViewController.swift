34c001c686d79ec4a96615b98e863f3c715f19ca
Bonkook Koo <djmimi@nate.com>
2019-01-04 12:10:12 +0900

    Init just new project single view app

---
+++ Twy222/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 04/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
