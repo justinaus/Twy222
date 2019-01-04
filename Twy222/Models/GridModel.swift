a50046683e9e275096753d460aa693649c7d03fe
Bonkook Koo <djmimi@nate.com>
2019-01-04 14:27:02 +0900

    Add GridModel and some test file

---
+++ Twy222/Models/GridModel.swift
@@ -0,0 +1,28 @@
+//
+//  GridModel.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 04/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import Foundation
+
+class GridModel {
+    public private(set) var latitude:String;
+    public private(set) var longitude:String;
+    
+    // 구분 키 값
+    public private(set) var id: String;
+    
+    public private(set) var dongName: String;
+    
+    init( id: String, lat: String, lon: String, dongName: String ) {
+        self.id = id;
+        self.latitude = lat;
+        self.longitude = lon;
+        self.dongName = dongName;
+    }
+    
+    
+}
---
+++ Twy222/Services/KakaoApiService.swift
@@ -0,0 +1,9 @@
+//
+//  KakaoApiService.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 04/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import Foundation
