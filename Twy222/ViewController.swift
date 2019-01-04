ceb4311a925612bc0ce53e94b5bc96248380f8bb
Bonkook Koo <djmimi@nate.com>
2019-01-04 13:48:50 +0900

    Add CoreLocation

--- Twy222/ViewController.swift
+++ Twy222/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager: CLLocationManager = CLLocationManager();
+    var currentLocation: CLLocation?;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("view did load");
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+//        locationManager.distanceFilter = 5;
+        
+        locationManager.requestWhenInUseAuthorization();
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self;
+            locationManager.startUpdatingLocation()
+        } else if status == .denied {
+            print("denied")
+//            goJustDefaultTempLocation();
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+//            goJustDefaultTempLocation();
+            
+            return;
+        }
+        
+        currentLocation = location;
+        
+        print("locationManager", location.coordinate.longitude, location.coordinate.latitude );
+        
+        //        getGridModelByLonLat( lon: location.coordinate.longitude, lat: location.coordinate.latitude );
+    }
 }
 
