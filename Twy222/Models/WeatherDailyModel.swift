ce1d3ba82f047cbeeb72a2a5fe7659100034967c
Bonkook Koo <djmimi@nate.com>
2019-01-09 01:55:01 +0900

    Add common models

---
+++ Twy222/Models/WeatherDailyModel.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherDailyModel.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 05/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import Foundation
+
+//class WeatherDailyModel {
+//    
+//    public private(set) var date:Date;
+//    
+////    최저기온, 최대기온.
+////    
+////    상태 (구름 조금)
+////    비 확률?
+////    키 값 가능?
+//    
+//    
+//    // 구분 키 값
+//    public private(set) var id: String;
+//    
+//    init( id: String, lat: String, lon: String, dongName: String ) {
+//        self.id = id;
+//        self.latitude = lat;
+//        self.longitude = lon;
+//        self.dongName = dongName;
+//    }
+//}
