8ffdb79f7532a9b24055432849915cbdcf041ee8
Bonkook Koo <djmimi@nate.com>
2019-01-09 01:54:34 +0900

    Update Kma enums

---
+++ Twy222/Services/Kma/KmaPtyEnum.swift
@@ -0,0 +1,16 @@
+//
+//  KmaPtyEnum.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 07/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import Foundation
+
+public enum KmaPtyEnum : Int {
+    // 없음
+    case NONE = 0;
+    // 비 1 / 비/눈(진눈개비) 2 / 눈 3
+    case RAINY, RAINY_AND_SNOWY, SNOWY;
+}
---
+++ Twy222/Services/Kma/KmaSkyEnum.swift
@@ -0,0 +1,16 @@
+//
+//  KmaSkyEnum.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 07/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import Foundation
+
+public enum KmaSkyEnum : Int {
+    // 맑음
+    case GOOD = 1;
+    // 구름조금 2 / 구름많음 3 / 흐림 4
+    case LITTLE_CLOUDY, QUITE_CLOUDY, CLOUDY;
+}
