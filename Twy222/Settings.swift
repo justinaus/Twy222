efba745f77f15e928d072c19522e619b986830d4
Bonkook Koo <djmimi@nate.com>
2019-01-12 13:20:08 +0900

    Add Settings

---
+++ Twy222/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 12/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import Foundation
+
+struct Settings {
+    static let HOURLY_DATA_COUNT = 8;
+}
+
