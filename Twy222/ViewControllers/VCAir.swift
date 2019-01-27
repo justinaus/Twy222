9afb8016f117161ef31ffe8da8d3f6a6b0c1fa0b
Bonkook Koo <djmimi@nate.com>
2019-01-27 13:42:42 +0900

    Add air viewcontroller

---
+++ Twy222/ViewControllers/VCAir.swift
@@ -0,0 +1,18 @@
+//
+//  VCAir.swift
+//  Twy222
+//
+//  Created by Bonkook Koo on 27/01/2019.
+//  Copyright © 2019 justinaus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VCAir: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        print(123)
+    }
+}
